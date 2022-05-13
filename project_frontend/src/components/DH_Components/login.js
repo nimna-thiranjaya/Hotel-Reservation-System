53ac1f7ce4da6f769d94d0e44ca1fafa9cc48d1b
Dimalka Rathnayake <89465728+IT20128418@users.noreply.github.com>
2022-05-13 21:13:30 +0530

    login and register css

--- project_frontend/src/components/DH_Components/login.js
+++ project_frontend/src/components/DH_Components/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from "axios";
+import image from "../../asserts/DH_Asserts/MctTzB.jpg"
 
 export default class Login extends Component {
     constructor(props) {
@@ -54,28 +55,82 @@ export default class Login extends Component {
         return (
           
                
-               <div>
+              //  <div>
   
-                      <form onSubmit={this.userLoginSubmit} name="form"> 
+              //         <form onSubmit={this.userLoginSubmit} name="form"> 
   
-                        <label  >Student ID</label>
-                        <input  type="text" name="username" placeholder="Enter your email" onChange={e => this.setState({ email: e.target.value })} required/> <br/><br/> 
-                        <label  >Password</label> 
-                        <input  type="password" name="password" placeholder="Enter your Password" onChange={e => this.setState({ pwd: e.target.value })} required/> 
-                        <br/>   
+              //           <label  >Student ID</label>
+              //           <input  type="text" name="username" placeholder="Enter your email" onChange={e => this.setState({ email: e.target.value })} required/> <br/><br/> 
+              //           <label  >Password</label> 
+              //           <input  type="password" name="password" placeholder="Enter your Password" onChange={e => this.setState({ pwd: e.target.value })} required/> 
+              //           <br/>   
                           
-                        <button type="submit" >Login</button> 
+              //           <button type="submit" >Login</button> 
                         
                           
-                      </form>
+              //         </form>
                
-                <center><label >Not Registered?</label> </center>
-                <center><li><a href="/signup" >Sign Up</a></li></center>
+              //   <center><label >Not Registered?</label> </center>
+              //   <center><li><a href="/signup" >Sign Up</a></li></center>
                 
               
-                </div>
+              //   </div>
+
+<div>
+             
+<section className="vh-200" style={{backgroundColor: "#D8BFD8"}}>
+  <div className="container py-5 h-100">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col col-xl-10">
+        <div className="card" style={{borderRadius: "1rem", marginBottom:"1rem"}}>
+          <div className="row g-0">
+            <div className="col-md-6 col-lg-5 d-none d-md-block">
+              <img src={image}
+                alt="login form" className="img-fluid" style={{borderRadius: "1rem 0 0 1rem" }}/>
+            </div>
+            <div className="col-md-6 col-lg-7 d-flex align-items-center">
+              <div className="card-body p-4 p-lg-5 text-black">
+
+                <form onSubmit={this.userLoginSubmit} name="form">
+
+                  <div className="d-flex align-items-center mb-3 pb-1">
+                    <i className="fas fa-cubes fa-2x me-3" style={{color: "#ff6219"}}></i>
+                    <span className="h1 fw-bold mb-0">Logo</span>
+                  </div>
+
+                  <h5 className="fw-normal mb-3 pb-3" style={{letterSpacing:"1px"}}>Sign into your account</h5>
+
+                  <div class="form-floating mb-3">
+                <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com" onChange={e => this.setState({ email: e.target.value })} required/>
+                <label for="floatingInput">Email address</label>
+              </div>
+
+              <div class="form-floating mb-3">
+                <input type="password" class="form-control" name="pwd" id="floatingInput" placeholder="Password" onChange={e => this.setState({ pwd: e.target.value })} required/>
+                <label for="floatingInput">Password</label>
+              </div>
+
+                  <div className="pt-1 mb-4">
+                    <button className="btn btn-dark btn-lg btn-block" type="submit">Login</button>
+                  </div>
+
+   
+                  <p className="mb-5 pb-lg-2" style={{color: "#393f81"}}>Don't have an account? <a href="/aa"
+                      style={{color: "#393f81"}}>Register here</a></p>
+
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
            
-  
+</div>
       
         )
       }
--- project_frontend/src/components/DH_Components/signup.js
+++ project_frontend/src/components/DH_Components/signup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from "axios";
 import FileBase64 from 'react-file-base64';
+import image from "../../asserts/DH_Asserts/ss.jpg"
 
 
 export default class Signup extends Component {
@@ -53,7 +54,7 @@ if(this.state.pwd === this.state.cpwd){
        
           <div>
               
-              <form name="form" onSubmit={this.onSubmit}> 
+              {/* <form name="form" onSubmit={this.onSubmit}> 
                              
                             <h1>Hotel Name</h1>
                                  <input type="text" name="hname"  placeholder="Hotel Name"
@@ -94,7 +95,87 @@ if(this.state.pwd === this.state.cpwd){
                                      Register
                                  </button></center>
                              
-              </form>     
+              </form>      */}
+
+
+<section className="text-center">
+  <div className="p-5 bg-image" style={{backgroundImage: `url(${image})`,height: "300px",backgroundSize: 'cover'}}></div>
+
+  <div className="card mx-4 mx-md-5 shadow-5-strong" style={{marginTop: "-130px", background: "hsla(0, 0%, 100%, 0.8)",backdropFilter:` blur(20px)`,marginBottom:"3rem"}}>
+    <div className="card-body py-5 px-md-5">
+
+      <div className="row d-flex justify-content-center">
+        <div className="col-lg-8">
+          <h2 className="fw-bold mb-5">Sign up now</h2>
+          <form  name="form" onSubmit={this.onSubmit}>
+
+
+            
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control" name="hname" id="floatingInput" placeholder="Hotel Name"  onChange={this.handleInputChange} value={this.setState.hname} required/>
+            <label for="floatingInput">Hotel Name</label>
+          </div>
+
+            <div className="row">
+              <div className="col-md-6 mb-4">
+              <div class="form-floating mb-3">
+                <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com" onChange={this.handleInputChange} value={this.setState.email} required/>
+                <label for="floatingInput">Email address</label>
+              </div>
+              </div>
+              <div className="col-md-6 mb-4">
+              <div class="form-floating mb-3">
+                <input type="phone" class="form-control" name="phone" id="floatingInput" placeholder="Phone Number" onChange={this.handleInputChange} value={this.setState.phone} required/>
+                <label for="floatingInput">Phone Number</label>
+              </div>
+              </div>
+            </div>
+
+
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="address" id="floatingInput" placeholder="Address" onChange={this.handleInputChange} value={this.setState.address} required/>
+                <label for="floatingInput">Address</label>
+              </div>
+
+            <div class="form-floating mb-3">
+              <textarea id="floatingInput" name="details" style={{height: "100px"}} type="text"  class="form-control" placeholder="Details"  onChange={this.handleInputChange} value={this.setState.details} required/>
+              <label for="floatingInput">Details</label>
+            </div>
+
+
+
+            <div className="row">
+              <div className="col-md-6 mb-4">
+              <div class="form-floating mb-3">
+                <input type="password" class="form-control" name="pwd" id="floatingInput" placeholder="Password" onChange={this.handleInputChange} value={this.setState.pwd} required/>
+                <label for="floatingInput">Password</label>
+              </div>
+              </div>
+              <div className="col-md-6 mb-4">
+              <div class="form-floating mb-3">
+                <input type="password" class="form-control" name="cpwd" id="floatingInput" placeholder="Password" onChange={this.handleInputChange} value={this.setState.cpwd} required/>
+                <label for="floatingInput">Confirm Password</label>
+              </div>
+              </div>
+            </div>
+
+
+            <div class="custom-file">
+            <h5><label class="custom-file-label" for="customFileLangHTML">Image : </label></h5><br/>
+            <FileBase64 type="file" class="custom-file-input" id="customFileLangHTML" name="image" multiple={ false } onDone={({ base64 }) => this.setState({ image: base64 })} hidden required/>
+            </div>
+
+            <br/><br/>
+
+            <button type="submit" className="btn btn-primary btn-block mb-4">
+              Sign up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
                                               
           </div>     
         
